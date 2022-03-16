393e7f8cdddb659b20e60410f5d40d9375e1a11d
Alex <a.shiryaev@ansaligy.com>
2022-03-16 10:55:36 +0300

    finish

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -5,6 +5,7 @@ $( document ).ready(function() {
 
     burgerBtn.on('click tap', function () {
         $(this).toggleClass('burger-on')
+        $(this).closest('.header__top').toggleClass('header-on')
         mobileMenu.toggleClass('menu-show')
         logo.toggleClass('mobile-logo')
         $('body').toggleClass('overflow')
